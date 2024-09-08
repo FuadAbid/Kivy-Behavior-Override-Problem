9a2b4af8d3ddaa67cc743051506356eb21afd1f1
Fuad <69778616+FuadAbid@users.noreply.github.com>
2024-09-08 12:12:02 +0600

    Update background_behavior.py

--- New_folder/behaviors/background_behavior.py
+++ New_folder/behaviors/background_behavior.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 __all__ = ("BackgroundBehavior",)
 
 from kivy.animation import Animation
@@ -13,41 +11,20 @@ from kivy.properties import (
 Builder.load_string("""
 #: import RelativeLayout kivy.uix.relativelayout.RelativeLayout
 
-
 <BackgroundBehavior>
     canvas.before:
         Color:
-            rgba: self._bg_color
+            rgba: self.bg_color
         SmoothRoundedRectangle:
             size: self.size
             pos: self.pos if not isinstance(self, RelativeLayout) else (0, 0)
             radius: self.radius
-            source: self.background
+
 """, 
 filename='background_bahvior.kv'
 )
 
 
 class BackgroundBehavior:
-    background = StringProperty()
-    radius = VariableListProperty([0, 0, 0, 0])
-    bg_color = ColorProperty()
-
-    _bg_color = ColorProperty([0, 0, 0, 0])
-
-    def on_bg_color(self, instance, color: list | str):
-        """Fired when the values of :attr:`bg_color` change."""
-        
-        if (
-            hasattr(self, "theme_cls")
-            and self.theme_cls.theme_style_switch_animation
-            and self.__class__.__name__ != "MDDropdownMenu"
-        ):
-            Animation(
-                _bg_color=color,
-                d=self.theme_cls.theme_style_switch_animation_duration,
-                t="linear",
-            ).start(self)
-        
-        else:
-            self._bg_color = color
+    radius = VariableListProperty([0,])
+    bg_color = ColorProperty([0, 0, 0, 0])
